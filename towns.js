28b03e8aefae17f02aa796e1e175e02ac28a5b58
ZheniKoleva <zhenie_koleva@abv.bg>
2023-11-06 19:40:38 +0200

    Add functionalities added

--- towns.js
+++ towns.js
@@ -2,6 +2,18 @@ $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
 });
 
+$(document).ready(function() {
+	…
+	$('#btnAdd').click(addTown);
+});
+
+function addTown() {
+	let townName = $('#townNameForAdd').val();
+	$('#townNameForAdd').val('');
+	$('#towns').append($('<option>').text(townName));
+	$('#result').text(townName + " added.");
+}
+
 function deleteTown() {
 	let townName = $('#townName').val();
 	$('#townName').val('');
